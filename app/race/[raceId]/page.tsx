caac99be6c41dd88c2ded9d596d84fe06e4b0d35
Nathan <nathanjoeldev@gmail.com>
2026-05-18 00:11:44 +0100

    Fix Uint8Array spread for Vercel TS target

--- app/race/[raceId]/page.tsx
+++ app/race/[raceId]/page.tsx
@@ -40,7 +40,7 @@ function generateUUID(): string {
   }
   arr[6] = (arr[6] & 0x0f) | 0x40;
   arr[8] = (arr[8] & 0x3f) | 0x80;
-  return [...arr].map((b, i) =>
+  return Array.from(arr).map((b, i) =>
     ([4, 6, 8, 10].includes(i) ? '-' : '') + b.toString(16).padStart(2, '0')
   ).join('');
 }
